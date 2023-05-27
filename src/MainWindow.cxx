754e48aa99cf28ec7db580e7737dc1948919cad3
Ethan Blahay <eblahay@protonmail.com>
2023-05-26 23:34:47 -0400

    mk it so window title contains img path

--- src/MainWindow.cxx
+++ src/MainWindow.cxx
@@ -1,7 +1,14 @@
 #include <eyren/MainWindow.hxx>
 
 eyren::MainWindow::MainWindow(std::filesystem::path path){
-    set_title("Eyren");
+    std::string title_str;
+    if(!path.empty()){
+        title_str += path;
+        title_str += " — ";
+    }
+    title_str += "Eyren";
+
+    set_title(title_str);
 
     // add widgets
 
